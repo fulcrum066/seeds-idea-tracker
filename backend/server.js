af809a8a7f0f2f5e03d74c0b5d77e147179f4c11
Melmakian2 <167946995+Melmakian2@users.noreply.github.com>
2025-10-08 19:41:44 +1100

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") }); // Load env immediately
+
+const express = require("express");
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// Port from .env or default to 5000
+const port = process.env.PORT || 5000;
+
+// Initialize express
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/seeds", require("./routes/seedRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
+
+// Error handler
+app.use(errorHandler);
+
+// Start server
+app.listen(port, () => console.log(`Server started on port ${port}`));
